cd0f570e7b5e8ad8789d1a8644116418f9a88e16
Geetika Gunjan <Geets@Geetikas-MacBook-Pro.local>
2018-05-15 22:52:35 -0700

    Detect Cycle in a LinkedList

---
+++ src/src/coding/LinkedListStackQueues/LinkedList.java
@@ -0,0 +1,56 @@
+package src.coding.LinkedListStackQueues;
+
+
+public class LinkedList {
+	LLNode head;
+	
+	
+	
+	void add_node_atHead (LLNode n) {
+		n.next = this.head;
+		this.head = n;
+	}
+	
+	@Override
+	public String toString() {
+		String result = "{ ";
+		
+		LLNode curr = this.head;
+		while (curr != null) {
+			result = result + curr.data +" ";
+			curr = curr.next;
+		}
+		result += " }";
+		return result;
+	}
+	
+
+ static void printLL(LLNode temp) {
+	 
+	 if ( temp == null) return;
+	 
+	 LLNode curr = temp;
+	 String result = " {";
+	 while ( curr != null ) {
+		 System.out.println("Helllo");
+		 result += curr.data + ", ";
+	 }
+	 System.out.println("printing current-> "+curr);
+	 result = result + " }";
+	 System.out.println(result);
+ }
+ 
+}
+
+ class LLNode {
+	int data;
+	LLNode next;
+	LLNode(int data) {
+		this.data  = data;
+	}
+	
+	@Override
+	public String toString() {
+		return "" + this.data;
+	}
+}
---
+++ src/src/coding/LinkedListStackQueues/LinkedListCycleDetection.java
@@ -0,0 +1,44 @@
+package src.coding.LinkedListStackQueues;
+
+public class LinkedListCycleDetection {
+
+	static boolean detect_cycle(LinkedList ll) {
+
+		// Return if linked list is null
+		if (ll == null || ll.head == null)
+			return false;
+
+		LLNode slow, fast;
+		slow = ll.head;
+		fast = ll.head;
+
+		// Detect if loop exist in a linked list
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if (slow == fast) {
+				removeLoop(slow, ll.head);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Remove the loop
+	static void removeLoop(LLNode slow, LLNode start) {
+
+		// LLNode head = start;
+		if (start == null || slow == null)
+			return;
+
+		while (start.next != slow.next) {
+			start = start.next;
+			slow = slow.next;
+		}
+
+		slow.next = null;
+
+	}
+
+}
